f33c184f289f23e293f683d49458101b35954833
mohdtabish999 <mohdtabish999@gmail.com>
2021-07-08 18:17:22 +0530

    added rest controller advice for feign exception

---
+++ src/main/java/com/example/wrapper/config/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.example.wrapper.config;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import feign.FeignException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(FeignException.class)
+	public String handleFeignStatusException(FeignException e, HttpServletResponse response) {
+		response.setStatus(e.status());
+		return "feignError";
+	}
+	
+
+}
